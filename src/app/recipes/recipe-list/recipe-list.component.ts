1f2d2e17e3a838818aa1bb57bd4fc41b92110e38
m.sawastian <m.sawastian@gmail.com>
2018-09-02 12:06:29 +0200

    Recipe display scaffolding.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Pancake', 'A pancake',
+      'http://pngimg.com/uploads/pancake/pancake_PNG93.png')
+  ];
 
   constructor() { }
 
