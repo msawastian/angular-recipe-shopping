6de3a8951b85e930070c90eeea5d87a27ca19888
m.sawastian <m.sawastian@gmail.com>
2018-09-06 15:57:45 +0200

    Changes to recipes, recipe list, recipe item and recipe detail. Switching recipes now changes the detail component.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,10 +8,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Pancake', 'A pancake',
       'http://pngimg.com/uploads/pancake/pancake_PNG93.png'),
-    new Recipe('Pancake', 'A pancake',
+    new Recipe('Another Pancake', 'A pancake',
       'http://pngimg.com/uploads/pancake/pancake_PNG93.png')
   ];
 
@@ -20,4 +21,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
