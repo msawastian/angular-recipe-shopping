c55a72ebae77e18f31cd65ae11c1306d938589d5
m.sawastian <m.sawastian@gmail.com>
2018-09-13 22:06:46 +0200

    Passing ingredients using services.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipesService} from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +10,12 @@ import {Recipe} from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,17 +1,36 @@
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipesService {
   public recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Pancake', 'A pancake',
-      'http://pngimg.com/uploads/pancake/pancake_PNG93.png'),
-    new Recipe('Another Pancake', 'A pancake',
-      'http://pngimg.com/uploads/pancake/pancake_PNG93.png')
+    new Recipe('Pancake',
+      'A pancake',
+      'http://pngimg.com/uploads/pancake/pancake_PNG93.png',
+      [
+        new Ingredient('Flour', 15),
+        new Ingredient('Milk', 2)
+      ]),
+    new Recipe('Another Pancake',
+      'A pancake',
+      'http://pngimg.com/uploads/pancake/pancake_PNG93.png',
+      [
+        new Ingredient('Flour', 23),
+        new Ingredient('Eggs', 3)
+      ])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice(); // returns a copy of the array
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
